17e7679a503fd2ebcb6b93480601ba0a0f1c3ea0
Lucas Santos <lhs.santoss@gmail.com>
2022-02-23 01:41:38 +0100

    wip: linux links
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- link-unix.sh
+++ link-unix.sh
@@ -11,5 +11,32 @@ DIRS="./Global/*
 ./MacOSX/*"
 for file in $DIRS; do
     echo "~> Linking $file to $HOME/$(basename $file)"
-    echo "would have ran: ln \"$file\" \"$HOME/$(basename $file)\""
+    echo "would have ran: ln -sf \"$file\" \"$HOME/$(basename $file)\""
 done
+
+# Install all from Brewfile
+
+# Install ASDF (already done in brewfile)
+# install asdf plugins
+asdf plugin-add ruby
+asdf plugin-add python
+asdf plugin-add nodejs
+asdf plugin-add golang
+asdf plugin-add rust
+
+# install asdf versions
+asdf install nodejs lts
+asdf install nodejs latest
+asdf install rust stable
+asdf install ruby "$(asdf list-all ruby | grep -e "^\d*\.\d*\.\d*$" | tail -n 1)"
+asdf install python "$(asdf list-all python | grep -e "^\d*\.\d*\.\d*$" | tail -n 1)"
+
+# set asdf global
+asdf global nodejs lts
+asdf global nodejs latest
+asdf global rust stable
+asdf global ruby "$(asdf list-all ruby | grep -e "^\d*\.\d*\.\d*$" | tail -n 1)"
+asdf global python "$(asdf list-all python | grep -e "^\d*\.\d*\.\d*$" | tail -n 1)"
+
+# Install lunarvim
+bash <(curl -s https://raw.githubusercontent.com/khaosdoctor/lunarvim/main/utils/installer/install.sh) -y
