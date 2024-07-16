e39dc761f32047d6672d40acd07d90faeb43e0aa
Lucas Santos <lhs.santoss@gmail.com>
2024-07-17 01:55:01 +0200

    feat(polybar): split polybars among monitors

--- arch/polybar/.config/polybar/launch_polybar.sh
+++ arch/polybar/.config/polybar/launch_polybar.sh
@@ -1,7 +1,12 @@
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
-  done
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload toph &
+#  done
+  PORTRAIT_MONITOR=$(xrandr --query | grep " connected" | grep "1440x2560" | cut -d" " -f1) \
+  polybar --reload portrait &
+
+  LANDSCAPE_MONITOR=$(xrandr --query | grep " connected" | grep "2560x1440" | cut -d" " -f1) \
+  polybar --reload landscape &
 else
   polybar --reload toph &
 fi
