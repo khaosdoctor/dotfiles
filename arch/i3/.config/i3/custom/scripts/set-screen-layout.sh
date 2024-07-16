f4a10d9e77fb720d3a9661a2249a02140f1210e7
Lucas Santos <lhs.santoss@gmail.com>
2024-07-16 19:11:08 +0200

    fix(i3): i3 not running startup scripts

--- arch/i3/.config/i3/custom/scripts/set-screen-layout.sh
+++ arch/i3/.config/i3/custom/scripts/set-screen-layout.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 if ! command -v xrandr &>/dev/null
 then
-    echo "[i3-custom-scripts] Couldn't find xrandr to set screen layout" | systemd-cat -p emerg
+    echo "[i3-custom-scripts] Couldn't find xrandr to set screen layout" | systemd-cat -p emerg -t $(pwd)
     exit 1
 fi
 xrandr --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate right --output DP-3 --off --output DP-4 --primary --mode 2560x1440 --pos 1440x560 --rotate normal --output DP-5 --off
 
-echo "[i3-custom-scripts] Monitor layout set up successfully" | systemd-cat -p info
+echo "[i3-custom-scripts] Monitor layout set up successfully" | systemd-cat -p info -t $(pwd)
