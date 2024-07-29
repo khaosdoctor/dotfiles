002d0ce0b6979f560e9111029636d0d0c325e111
Lucas Santos <lhs.santoss@gmail.com>
2024-07-30 00:13:47 +0200

    feat: add lsp configuration for ts and deno

--- general/nvim/.config/nvim/lua/plugins/lspconfigs.lua
+++ general/nvim/.config/nvim/lua/plugins/lspconfigs.lua
@@ -27,79 +27,209 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.bashls.setup({})
-      lspconfig.cssls.setup({})
-
-      lspconfig.dockerls.setup({})
-      lspconfig.docker_compose_language_service.setup({})
-      lspconfig.eslint.setup({})
-      lspconfig.emmet_ls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.jqls.setup({})
-      lspconfig.ltex.setup({})
-      lspconfig.marksman.setup({})
-      lspconfig.taplo.setup({})
-      lspconfig.volar.setup({})
-      lspconfig.denols.setup({
-        root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-        init_options = {
-          enable = true,
-          lint = true,
-          unstable = true,
-          suggest = {
-            imports = {
-              hosts = {
-                "https://deno.land",
-                "https://jsr.io",
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        lua_ls = {},
+        bashls = {},
+        cssls = {},
+        dockerls = {},
+        docker_compose_language_service = {},
+        eslint = {},
+        emmet_ls = {},
+        cssmodules_ls = {},
+        css_variables = {},
+        html = {},
+        jsonls = {},
+        jqls = {},
+        ltex = {},
+        marksman = {},
+        taplo = {},
+        volar = {},
+        denols = {
+          -- Prevents deno from being loaded into node projects
+          root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
+          --   local lspconfig = require("lspconfig")
+          --   return lspconfig.util.root_pattern("deno.json", "deno.jsonc")
+          -- end,
+          init_options = {
+            enable = true,
+            lint = true,
+            unstable = true,
+            suggest = {
+              imports = {
+                hosts = {
+                  "https://deno.land",
+                  "https://jsr.io",
+                },
               },
             },
           },
         },
-        on_attach = on_attach,
-      })
-      lspconfig.tsserver.setup({
-        init_options = {
-          plugins = {
+        tsserver = {
+          enabled = false,
+        },
+        vtsls = {
+          -- explicitly add default filetypes, so that we can extend
+          -- them in related extras
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
+          root_dir = function(filename)
+            local lspconfig = require("lspconfig")
+            local denoRootDir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
+            -- prevents tsserver from attaching in deno projects
+            if denoRootDir then
+              return nil
+            end
+            return lspconfig.util.root_pattern("package.json")(filename)
+          end,
+          settings = {
+            complete_function_calls = true,
+            vtsls = {
+              enableMoveToFileCodeAction = true,
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = "literals" },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
+          keys = {
             {
-              name = "@vue/typescript-plugin",
-              location = "/usr/lib/node_modules/@vue/typescript-plugin",
-              languages = { "javascript", "typescript", "vue" },
+              "gD",
+              function()
+                local params = vim.lsp.util.make_position_params()
+                LazyVim.lsp.execute({
+                  command = "typescript.goToSourceDefinition",
+                  arguments = { params.textDocument.uri, params.position },
+                  open = true,
+                })
+              end,
+              desc = "Goto Source Definition",
+            },
+            {
+              "gR",
+              function()
+                LazyVim.lsp.execute({
+                  command = "typescript.findAllFileReferences",
+                  arguments = { vim.uri_from_bufnr(0) },
+                  open = true,
+                })
+              end,
+              desc = "File References",
+            },
+            {
+              "<leader>co",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
+            },
+            {
+              "<leader>cM",
+              LazyVim.lsp.action["source.addMissingImports.ts"],
+              desc = "Add missing imports",
+            },
+            {
+              "<leader>cu",
+              LazyVim.lsp.action["source.removeUnused.ts"],
+              desc = "Remove unused imports",
+            },
+            {
+              "<leader>cD",
+              LazyVim.lsp.action["source.fixAll.ts"],
+              desc = "Fix all diagnostics",
+            },
+            {
+              "<leader>cV",
+              function()
+                LazyVim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
+              end,
+              desc = "Select TS workspace version",
             },
           },
         },
-        filetypes = {
-          "javascript",
-          "javascriptreact",
-          "javascript.jsx",
-          "typescript",
-          "typescriptreact",
-          "typescript.tsx",
-          "vue",
-        },
-        on_attach = function(client, bufnr)
-          on_attach(client, bufnr)
-          vim.keymap.set("n", "<leader>rn", function()
-            vim.lsp.buf.execute_command({
-              command = "_typescript.organizeImports",
-              arguments = { vim.fn.expand("%:p") },
-            })
-          end, { buffer = bufnr, remap = false })
+      },
+      setup = {
+        tsserver = function()
+          -- disable tsserver
+          return true
         end,
-        root_dir = function(filename, bufnr)
-          local denoRootDir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
-          -- prevents tsserver from attaching in deno projects
-          if denoRootDir then
-            return nil
-          end
-          return lspconfig.util.root_pattern("package.json")(filename)
+        vtsls = function(_, opts)
+          LazyVim.lsp.on_attach(function(client, buffer)
+            client.commands["_typescript.moveToFileRefactoring"] = function(command, ctx)
+              ---@type string, string, lsp.Range
+              local action, uri, range = unpack(command.arguments)
+
+              local function move(newf)
+                client.request("workspace/executeCommand", {
+                  command = command.command,
+                  arguments = { action, uri, range, newf },
+                })
+              end
+
+              local fname = vim.uri_to_fname(uri)
+              client.request("workspace/executeCommand", {
+                command = "typescript.tsserverRequest",
+                arguments = {
+                  "getMoveToRefactoringFileSuggestions",
+                  {
+                    file = fname,
+                    startLine = range.start.line + 1,
+                    startOffset = range.start.character + 1,
+                    endLine = range["end"].line + 1,
+                    endOffset = range["end"].character + 1,
+                  },
+                },
+              }, function(_, result)
+                ---@type string[]
+                local files = result.body.files
+                table.insert(files, 1, "Enter new path...")
+                vim.ui.select(files, {
+                  prompt = "Select move destination:",
+                  format_item = function(f)
+                    return vim.fn.fnamemodify(f, ":~:.")
+                  end,
+                }, function(f)
+                  if f and f:find("^Enter new path") then
+                    vim.ui.input({
+                      prompt = "Enter move destination:",
+                      default = vim.fn.fnamemodify(fname, ":h") .. "/",
+                      completion = "file",
+                    }, function(newf)
+                      return newf and move(newf)
+                    end)
+                  elseif f then
+                    move(f)
+                  end
+                end)
+              end)
+            end
+          end, "vtsls")
+          -- copy typescript settings to javascript
+          opts.settings.javascript =
+            vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
         end,
-      })
-      lspconfig.cssmodules_ls.setup({})
-      lspconfig.css_variables.setup({})
-    end,
+      },
+    },
   },
 }
