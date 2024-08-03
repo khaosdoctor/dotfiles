818f13882651bab01635ae62935f81ccfc48eeb2
Lucas Santos <lhs.santoss@gmail.com>
2024-08-03 13:55:08 +0200

    feat: notify on update

--- general/nvim/.config/nvim/lua/config/lazy.lua
+++ general/nvim/.config/nvim/lua/config/lazy.lua
@@ -37,7 +37,7 @@ require("lazy").setup({
   install = { colorscheme = { "tokyonight", "habamax", "catpuccin" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
+    notify = true, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
     rtp = {
