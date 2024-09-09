ca31205bf55e562a976aada6c6c634a391bedf52
Lucas Santos <hello@lsantos.dev>
2024-09-09 13:34:04 +0200

    fix: remove the new ts integration

--- general/nvim/.config/nvim/lua/config/lazy.lua
+++ general/nvim/.config/nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
-    { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.json" },
