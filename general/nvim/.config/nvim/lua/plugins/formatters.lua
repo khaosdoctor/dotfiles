bb9065553e53f744f592f572ebce4aefe0e7d262
Lucas Santos <hello@lsantos.dev>
2024-11-20 01:30:22 +0100

    chore: general updates

--- general/nvim/.config/nvim/lua/plugins/formatters.lua
+++ general/nvim/.config/nvim/lua/plugins/formatters.lua
@@ -1,18 +1,20 @@
 return {
-  "stevearc/conform.nvim",
-  dependencies = { "mason.nvim" },
-  lazy = false,
-  cmd = "ConformInfo",
-  opts = {
-    formatters_by_ft = {
-      rust = { "rustfmt" },
-      markdown = { "prettierd" },
-      sh = { "shfmt" },
-      bash = { "shfmt" },
-      shell = { "shfmt" },
-      lua = { "stylua" },
-      python = { "ruff" },
-      go = { "gofmt" },
+  {
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
+    lazy = false,
+    cmd = "ConformInfo",
+    opts = {
+      formatters_by_ft = {
+        rust = { "rustfmt" },
+        markdown = { "prettierd" },
+        sh = { "shfmt" },
+        bash = { "shfmt" },
+        shell = { "shfmt" },
+        lua = { "stylua" },
+        python = { "ruff" },
+        go = { "gofmt" },
+      },
     },
   },
 }
