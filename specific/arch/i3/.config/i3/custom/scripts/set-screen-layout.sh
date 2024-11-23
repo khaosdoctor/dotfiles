d9a3cfcf69018cac6dd5019a48713f39d0d0f223
Lucas Santos <lhs.santoss@gmail.com>
2024-11-23 02:19:32 +0100

    fix: increase screen scale

--- specific/arch/i3/.config/i3/custom/scripts/set-screen-layout.sh
+++ specific/arch/i3/.config/i3/custom/scripts/set-screen-layout.sh
@@ -3,6 +3,6 @@ if ! command -v xrandr &>/dev/null; then
     echo "[i3-custom-scripts] Couldn't find xrandr to set screen layout" | systemd-cat -p emerg -t $(pwd)
     exit 1
 fi
-xrandr --dpi 120 --output DP-0 --mode 2560x1440 --scale 1x1 --pos 3840x415 --rotate normal --output DP-2 --primary --mode 3840x2160 --pos 0x0 -r 240 --rotate normal
 
+xrandr --dpi 120 --output DP-0 --mode 2560x1440 --scale 1x1 --pos 3840x415 --rotate normal --output DP-2 --primary --mode 3840x2160 --scale 0.85x0.85 --pos 0x0 -r 240 --rotate normal
 echo "[i3-custom-scripts] Monitor layout set up successfully" | systemd-cat -p info -t $(pwd)
