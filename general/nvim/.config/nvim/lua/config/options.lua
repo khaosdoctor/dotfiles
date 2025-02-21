1f19b637d1d83c58dbb492a6c4eac193ea926731
Lucas Santos <hello@lsantos.dev>
2025-02-21 15:32:01 +0100

    fix: disable nvim animations

--- general/nvim/.config/nvim/lua/config/options.lua
+++ general/nvim/.config/nvim/lua/config/options.lua
@@ -5,6 +5,7 @@
 -- -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = true
+vim.g.snacks_animate = false
 -- Auto wrap
 vim.opt.wrap = true
 -- Attempt to fix indent
