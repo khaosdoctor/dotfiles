22e4c3bde9089e6807ef9a26874f6b7a0acc95a0
Lucas Santos <hello@lsantos.dev>
2025-02-28 13:22:26 +0100

    fix(lazy): configure dashboard with snacks

--- general/nvim/.config/nvim/lua/plugins/dashboard.lua
+++ general/nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -10,34 +10,24 @@ return {
     enabled = true,
     init = false,
     opts = function()
-      print(LazyVim)
       local dashboard = require("alpha.themes.dashboard")
-      --local logo = [[
-      --  ██╗     ███████╗ █████╗ ███╗   ██╗████████╗ ██████╗ ███████╗   ██████╗ ███████╗██╗   ██╗
-      --  ██║     ██╔════╝██╔══██╗████╗  ██║╚══██╔══╝██╔═══██╗██╔════╝   ██╔══██╗██╔════╝██║   ██║
-      --  ██║     ███████╗███████║██╔██╗ ██║   ██║   ██║   ██║███████╗   ██║  ██║█████╗  ██║   ██║
-      --  ██║     ╚════██║██╔══██║██║╚██╗██║   ██║   ██║   ██║╚════██║   ██║  ██║██╔══╝  ╚██╗ ██╔╝
-      --  ███████╗███████║██║  ██║██║ ╚████║   ██║   ╚██████╔╝███████║██╗██████╔╝███████╗ ╚████╔╝
-      --  ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝    ╚═════╝ ╚══════╝╚═╝╚═════╝ ╚══════╝  ╚═══╝
-      --]]
-
       local logo = [[
-        ██╗██╗     ███████╗   ██████╗ ███████╗██╗   ██╗██╗  
-       ██╔╝██║     ██╔════╝   ██╔══██╗██╔════╝██║   ██║╚██╗ 
+        ██╗██╗     ███████╗   ██████╗ ███████╗██╗   ██╗██╗
+       ██╔╝██║     ██╔════╝   ██╔══██╗██╔════╝██║   ██║╚██╗
       ██╔╝ ██║     ███████╗   ██║  ██║█████╗  ██║   ██║ ╚██╗
       ╚██╗ ██║     ╚════██║   ██║  ██║██╔══╝  ╚██╗ ██╔╝ ██╔╝
-       ╚██╗███████╗███████║██╗██████╔╝███████╗ ╚████╔╝ ██╔╝ 
-        ╚═╝╚══════╝╚══════╝╚═╝╚═════╝ ╚══════╝  ╚═══╝  ╚═╝  
-                                                            
+       ╚██╗███████╗███████║██╗██████╔╝███████╗ ╚████╔╝ ██╔╝
+        ╚═╝╚══════╝╚══════╝╚═╝╚═════╝ ╚══════╝  ╚═══╝  ╚═╝
+
       ]]
       dashboard.section.header.val = vim.split(logo, "\n")
       -- stylua: ignore
       dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file",       ":Telescope find_files<cr>"),
+        dashboard.button("f", " " .. " Find file",       "<cmd>lua LazyVim.pick()()<cr>"),
         dashboard.button("n", " " .. " New file",        [[<cmd> ene <BAR> startinsert <cr>]]),
-        dashboard.button("r", " " .. " Recent files",    "<cmd>Telescope oldfiles<cr>"),
-        dashboard.button("g", " " .. " Find text",       "<cmd>Telescope live_grep<cr>"),
-        dashboard.button("c", " " .. " Config",          "<cmd>lua require'telescope.builtin'.find_files({cwd = '~/.config/nvim'})<cr>"),
+        dashboard.button("r", " " .. " Recent files",    "<cmd>lua LazyVim.pick('oldfiles')()<cr>"),
+        dashboard.button("g", " " .. " Find text",       "<cmd>lua LazyVim.pick('live_grep')()<cr>"),
+        dashboard.button("c", " " .. " Config",          "<cmd>lua LazyVim.pick.config_files()()<cr>"),
         dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
         dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
         dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
