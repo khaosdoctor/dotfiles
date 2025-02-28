25d772fb7af7c78c3b3408048db4d347756f1cb8
Lucas Santos <hello@lsantos.dev>
2025-02-28 12:49:40 +0100

    fix: remove telescope to snacks

--- general/nvim/.config/nvim/lua/plugins/snacks.lua
+++ general/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -6,4 +6,9 @@ return {
       render = "fancy",
     },
   },
+  keys = {
+    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (Root Dir)" },
+    { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (Root Dir)" },
+    { "<leader>fF", LazyVim.pick("files", { root = true }), desc = "Find Files (Root Dir)" },
+  },
 }
--- general/nvim/.config/nvim/lua/plugins/telescope.lua
+++ general/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,17 @@
-return {
-  "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
-      path_display = { shorten = {
-        len = 5,
-        exclude = { 2, -1 },
-      } },
-    },
-  },
-  keys = {
-    { "<leader>ff", LazyVim.pick("files"), { root = true }, desc = "Find files (from root)" },
-    { "<leader>fF", LazyVim.pick("files"), { root = false }, desc = "Find files (cwd)" },
-  },
-}
+return {}
+-- return {
+--   "nvim-telescope/telescope.nvim",
+--   opts = {
+--     defaults = {
+--       path_display = { shorten = {
+--         len = 5,
+--         exclude = { 2, -1 },
+--       } },
+--     },
+--   },
+--   keys = {
+--     { "<leader>fF", LazyVim.pick("files"), { root = true }, desc = "Find files (from root)" },
+--     { "<leader>ff", LazyVim.pick("files"), { root = true }, desc = "Find files (cwd)" },
+--     { "<leader><space>", LazyVim.pick("files"), { root = true }, desc = "Find files (cwd)" },
+--   },
+-- }
