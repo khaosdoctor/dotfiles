0dbf241fc01c73b7c0fcce598f326ad104f37a0c
Lucas Santos <lhs.santoss@gmail.com>
2025-03-21 03:19:38 +0100

    fix: copyq with wofi

--- specific/arch/sway/.config/sway/custom/scripts/wofi-copyq.sh
+++ specific/arch/sway/.config/sway/custom/scripts/wofi-copyq.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-copyq eval -- "tab('&clipboard'); for(i=size(); i>0; --i) print(str(read(i-1)) + '\n');" | tac | awk NF | wofi -d
+copyq eval -- "tab('&clipboard'); for(i=size(); i>0; --i) print(str(read(i-1)) + '\n');" | tac | awk NF | wofi -d | wl-copy
