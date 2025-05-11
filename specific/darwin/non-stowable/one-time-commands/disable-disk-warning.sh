ac4905572d381e23cc0e0a4008559cd7d268ba5e
Lucas Santos <lhs.santoss@gmail.com>
2025-05-11 16:02:15 +0200

    fix: cleanup darwin

--- specific/darwin/non-stowable/one-time-commands/disable-disk-warning.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification -bool YES && sudo pkill diskarbitrationd
