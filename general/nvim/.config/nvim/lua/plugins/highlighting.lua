6accbd5964f80237c9508b2873f1c106bf793d8e
Lucas Santos <lucas.santos@hemnet.se>
2025-08-07 16:06:54 +0200

    feat: add nvim-highlight-colors instead of mini-hipatterns

--- general/nvim/.config/nvim/lua/plugins/highlighting.lua
+++ general/nvim/.config/nvim/lua/plugins/highlighting.lua
@@ -1,3 +1,17 @@
 return {
   { "isobit/vim-caddyfile" },
+  {
+    "brenoprata10/nvim-highlight-colors",
+    opts = {
+      render = "background",
+      enable_hex = true,
+      enable_rgb = true,
+      enable_hsl = true,
+      enable_ansi = true,
+      enable_hsl_without_function = true,
+      enable_var_usage = true,
+      enable_named_colors = true,
+      enable_tailwind = true,
+    },
+  },
 }
