d44e9770e225c51a13b3bf9f455ecea88b97c7a0
Lucas Santos <lhs.santoss@gmail.com>
2025-08-11 21:02:08 +0200

    fix: remove root dir from lsp configs
    This was causing errors in TS projects where the TS compiler could not
    pick up the root path. Removing it and letting it resolve is working
    fine, however I'm not sure if Deno projects will suffer from a problem
    later on since this was the thing preventing the LSP to start twice in
    the same file

--- general/nvim/.config/nvim/lua/plugins/lspconfigs.lua
+++ general/nvim/.config/nvim/lua/plugins/lspconfigs.lua
@@ -111,20 +111,20 @@ return {
             "typescriptreact",
             "typescript.tsx",
           },
-          root_dir = function(filename)
-            -- NOTE: root_pattern returns a function that needs to be called with the filename
-            local lspconfig = require("lspconfig")
-            local denoRootDir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")(filename)
-            -- prevents tsserver from attaching in deno projects
-            if denoRootDir then
-              return nil
-            end
-            local jsRoot = lspconfig.util.root_pattern("package.json")(filename)
-            if jsRoot then
-              return jsRoot
-            end
-            return filename
-          end,
+          -- root_dir = function(filename)
+          --   -- NOTE: root_pattern returns a function that needs to be called with the filename
+          --   local lspconfig = require("lspconfig")
+          --   local denoRootDir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")(filename)
+          --   -- prevents tsserver from attaching in deno projects
+          --   if denoRootDir then
+          --     return nil
+          --   end
+          --   local jsRoot = lspconfig.util.root_pattern("package.json")(filename)
+          --   if jsRoot then
+          --     return jsRoot
+          --   end
+          --   return filename
+          -- end,
           settings = {
             complete_function_calls = true,
             vtsls = {
@@ -151,58 +151,58 @@ return {
               },
             },
           },
-          keys = {
-            {
-              "gD",
-              function()
-                local params = vim.lsp.util.make_position_params()
-                LazyVim.lsp.execute({
-                  command = "typescript.goToSourceDefinition",
-                  arguments = { params.textDocument.uri, params.position },
-                  open = true,
-                })
-              end,
-              desc = "Goto Source Definition",
-            },
-            {
-              "gR",
-              function()
-                LazyVim.lsp.execute({
-                  command = "typescript.findAllFileReferences",
-                  arguments = { vim.uri_from_bufnr(0) },
-                  open = true,
-                })
-              end,
-              desc = "File References",
-            },
-            {
-              "<leader>co",
-              LazyVim.lsp.action["source.organizeImports"],
-              desc = "Organize Imports",
-            },
-            {
-              "<leader>cM",
-              LazyVim.lsp.action["source.addMissingImports.ts"],
-              desc = "Add missing imports",
-            },
-            {
-              "<leader>cu",
-              LazyVim.lsp.action["source.removeUnused.ts"],
-              desc = "Remove unused imports",
-            },
-            {
-              "<leader>cD",
-              LazyVim.lsp.action["source.fixAll.ts"],
-              desc = "Fix all diagnostics",
-            },
-            {
-              "<leader>cV",
-              function()
-                LazyVim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
-              end,
-              desc = "Select TS workspace version",
-            },
-          },
+          --   keys = {
+          --     {
+          --       "gD",
+          --       function()
+          --         local params = vim.lsp.util.make_position_params()
+          --         LazyVim.lsp.execute({
+          --           command = "typescript.goToSourceDefinition",
+          --           arguments = { params.textDocument.uri, params.position },
+          --           open = true,
+          --         })
+          --       end,
+          --       desc = "Goto Source Definition",
+          --     },
+          --     {
+          --       "gR",
+          --       function()
+          --         LazyVim.lsp.execute({
+          --           command = "typescript.findAllFileReferences",
+          --           arguments = { vim.uri_from_bufnr(0) },
+          --           open = true,
+          --         })
+          --       end,
+          --       desc = "File References",
+          --     },
+          --     {
+          --       "<leader>co",
+          --       LazyVim.lsp.action["source.organizeImports"],
+          --       desc = "Organize Imports",
+          --     },
+          --     {
+          --       "<leader>cM",
+          --       LazyVim.lsp.action["source.addMissingImports.ts"],
+          --       desc = "Add missing imports",
+          --     },
+          --     {
+          --       "<leader>cu",
+          --       LazyVim.lsp.action["source.removeUnused.ts"],
+          --       desc = "Remove unused imports",
+          --     },
+          --     {
+          --       "<leader>cD",
+          --       LazyVim.lsp.action["source.fixAll.ts"],
+          --       desc = "Fix all diagnostics",
+          --     },
+          --     {
+          --       "<leader>cV",
+          --       function()
+          --         LazyVim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
+          --       end,
+          --       desc = "Select TS workspace version",
+          --     },
+          --   },
         },
       },
       setup = {
