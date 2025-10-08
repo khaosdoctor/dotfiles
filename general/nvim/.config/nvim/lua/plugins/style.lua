87faeab29fb64f31ed6b20c4c8ba2b7aed5d7b78
Lucas Santos <lucas.santos@hemnet.se>
2025-10-08 13:25:58 +0200

    fix: remove smearing on insert

--- general/nvim/.config/nvim/lua/plugins/style.lua
+++ general/nvim/.config/nvim/lua/plugins/style.lua
@@ -1,5 +1,7 @@
 -- https://github.com/sphamba/smear-cursor.nvim/blob/main/lua/smear_cursor/config.lua
 return {
   "sphamba/smear-cursor.nvim",
-  opts = {},
+  opts = {
+    smear_insert_mode = false,
+  },
 }
