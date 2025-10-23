d7b028525b1d4ba6b81708c01a7aca24138aa32f
Lucas Santos <lucas.santos@hemnet.se>
2025-10-23 16:11:31 +0200

    fix: add sensible defaults to smearing
    even though they seem not to have any effect at all

--- general/nvim/.config/nvim/lua/plugins/style.lua
+++ general/nvim/.config/nvim/lua/plugins/style.lua
@@ -3,5 +3,8 @@ return {
   "sphamba/smear-cursor.nvim",
   opts = {
     smear_insert_mode = false,
+    delay_disable = 500,
+    min_horizontal_distance_smear = 20,
+    min_vertical_distance_smear = 20,
   },
 }
