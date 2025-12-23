d4a26b24eb92f0aafa41e177c20ea4ad1bc304f3
Lucas Santos <lucas.santos@hemnet.se>
2025-12-23 18:02:25 +0100

    feat: add keymap to disable completions

--- general/nvim/.config/nvim/lua/plugins/completions.lua
+++ general/nvim/.config/nvim/lua/plugins/completions.lua
@@ -11,6 +11,19 @@ return {
     opts.keymap["<C-u>"] = { "scroll_documentation_up", "fallback" }
     opts.keymap["<Tab>"] = { "select_and_accept", "fallback" }
 
+    -- Add enabled function to check global toggle state
+    -- This function is called by blink.cmp before showing completions
+    -- By returning false, we prevent completions from appearing at all
+    opts.enabled = function()
+      -- vim.g.blink_cmp_enabled is our global toggle state
+      -- nil or true = enabled, false = disabled
+      -- We explicitly check for false to default to enabled
+      if vim.g.blink_cmp_enabled == false then
+        return false -- Completions are disabled
+      end
+      return true -- Completions are enabled (default)
+    end
+
     -- Integration with nvim-highlight-colors
     opts.completion.menu.draw.components = opts.completion.menu.draw.components or {}
     opts.completion.menu.draw.components.kind_icon = opts.completion.menu.draw.components.kind_icon or {}
@@ -43,4 +56,38 @@ return {
 
     return opts
   end,
+  keys = {
+    {
+      "<leader>uB",
+      function()
+        -- Toggle completions on/off
+        -- This works by flipping a global variable that the enabled function checks
+        local cmp = require("blink.cmp")
+
+        -- Initialize the toggle state if it doesn't exist yet
+        -- Default to enabled (true)
+        if vim.g.blink_cmp_enabled == nil then
+          vim.g.blink_cmp_enabled = true
+        end
+
+        -- Flip the toggle state
+        vim.g.blink_cmp_enabled = not vim.g.blink_cmp_enabled
+
+        if vim.g.blink_cmp_enabled then
+          -- Completions are now enabled
+          -- Call reload() to re-trigger completion in the current context
+          cmp.reload()
+          vim.notify("Completions enabled", vim.log.levels.INFO, { title = "Blink" })
+        else
+          -- Completions are now disabled
+          -- Call hide() to immediately close any open completion menu
+          -- The enabled function above will prevent new menus from opening
+          cmp.hide()
+          vim.notify("Completions disabled", vim.log.levels.INFO, { title = "Blink" })
+        end
+      end,
+      mode = { "n", "i" }, -- Works in both normal and insert mode
+      desc = "Toggle Completions",
+    },
+  },
 }
