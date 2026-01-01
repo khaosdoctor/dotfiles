ac22194b8669d5b7e7ab5a9e4a1e4bd1a224d14a
Lucas Santos <lhs.santoss@gmail.com>
2026-01-01 20:12:31 +0100

    fix: remove smear since it causes more trouble than anything else

--- general/nvim/.config/nvim/lua/plugins/style.lua
+++ general/nvim/.config/nvim/lua/plugins/style.lua
@@ -1,10 +1,11 @@
 -- https://github.com/sphamba/smear-cursor.nvim/blob/main/lua/smear_cursor/config.lua
-return {
-  "sphamba/smear-cursor.nvim",
-  opts = {
-    smear_insert_mode = false,
-    delay_disable = 500,
-    min_horizontal_distance_smear = 20,
-    min_vertical_distance_smear = 20,
-  },
-}
+return {}
+-- return {
+--   "sphamba/smear-cursor.nvim",
+--   opts = {
+--     smear_insert_mode = false,
+--     delay_disable = 500,
+--     min_horizontal_distance_smear = 20,
+--     min_vertical_distance_smear = 20,
+--   },
+-- }
