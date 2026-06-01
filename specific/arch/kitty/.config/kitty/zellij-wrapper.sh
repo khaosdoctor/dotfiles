93e71adf546565bcdf55d2d7f477372c4c23bb12
Lucas Santos <lucas.santos@hemnet.se>
2026-06-01 15:59:28 +0200

    chore: fix wrappers

--- specific/arch/kitty/.config/kitty/zellij-wrapper.sh
+++ specific/arch/kitty/.config/kitty/zellij-wrapper.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env zsh
-SESSION="kitty-$$"
+export PATH="/usr/local/bin:$HOME/.cargo/bin:$HOME/.local/bin:$PATH"
+
+if [ "$TERM_PROGRAM" = "ghostty" ]; then
+    term=ghostty
+elif [ "$TERM_PROGRAM" = "rio" ]; then
+    term=rio
+elif [ -n "$KITTY_WINDOW_ID" ]; then
+    term=kitty
+else
+    term=term
+fi
+
+SESSION="${term}-$$"
 trap 'zellij delete-session "$SESSION" --force 2>/dev/null' EXIT
 zellij attach --create "$SESSION"
--- specific/darwin/kitty/.config/kitty/start-zellij.sh
+++ specific/darwin/kitty/.config/kitty/start-zellij.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+export PATH="/opt/homebrew/bin:/usr/local/bin:$HOME/.cargo/bin:$HOME/.local/bin:$PATH"
+
 if [ "$TERM_PROGRAM" = "ghostty" ]; then
     term=ghostty
+elif [ "$TERM_PROGRAM" = "rio" ]; then
+    term=rio
 elif [ -n "$KITTY_WINDOW_ID" ]; then
     term=kitty
 else
     term=term
 fi
-exec $(which zellij) attach --create "${term}-$$"
+exec zellij attach --create "${term}-$$"
